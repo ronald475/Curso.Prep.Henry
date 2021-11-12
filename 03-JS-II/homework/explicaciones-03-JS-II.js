4496c5c65af8885094846d7af38f37c912ad40a4
ronald475 <rdominguez2475@gmail.com>
2021-11-12 12:05:43 -0300

    Adding new file, explicaciones-03-JS-II.js

---
+++ 03-JS-II/homework/explicaciones-03-JS-II.js
@@ -0,0 +1,24 @@
+// &&
+    // El doble signo && se utiliza en la programacion
+    // como la letra "y" se utiliza en una oracion.
+    // Sirve para agregar elementos a las ideas que deseas expresar
+    // de modo que todos esos elementos formen parte de lo que vas a decir.
+    // Por ejemplo: puedes saber cuando sera la celebracion de tu cumpleaños
+    // si reunes el "dia" && "mes" de tu nacimento.
+
+// ||
+    // El doble signo || se utiliza como normalmente
+    // usarias la letra "o" en una oracion.
+    // Te puede servir para agregar elementos a una oracion
+    // para que al menos uno de ellos te ayude a expresar lo que deseas.
+    // Por ejemplo: cuando vas a comer una helado, puedes elegir entre muchos sabores
+    // pero probablemente elijas algunos de tus favoritos; vainilla || chocolate.
+    
+// !
+    // El simbolo ! se utiliza antes de las expresiones para cambiar
+    // el sentido de estas a su opuesto, es decir lo contrario
+    // a lo que originalmente significaban.
+    // Podrias usarla al escribir alguno de tus gustos
+    // y asi significaria que realmente no te gusta lo que escribiste.
+    // Por ejemplo: si escribes !MeGustaElHelado, estas diciendo 
+    // que realmente no te gusta el helado.
