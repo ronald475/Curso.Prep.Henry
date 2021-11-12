dedd65cfb53dd843f6762bc05d5afbedc5670347
ronald475 <rdominguez2475@gmail.com>
2021-11-12 09:05:35 -0300

    Adding new file, 02-JS-I/explicaciones-02-JS-I.js

---
+++ 02-JS-I/explicaciones-02-JS-I.js
@@ -0,0 +1,37 @@
+// Variables:
+    // Las variables son valores que escribimos ahora para poder utilizarlas cuando sea necesario.
+    // Por ejemplo: si quiero saber dentro de cuanto tiempo volvere a comer,
+    // y se que en este momento son las 10 horas del dia,
+    // solo necesito la variable almuerzo, que fue preestablecida entre las 12 y 13 horas,
+    // para calcular que volvere a comer en las siguientes 2 o 3 horas.
+
+// Strings:
+    // Las strings son cadenas de caracteres.
+    // Pueden incluir muchos caracteres, y a veces forman palabras simples,
+    // pero otras veces pueden ser mas dificiles de entender.
+    // Por ejemplo: la palabra pizza se escribe normalmente con una i y una a,
+    // pero un string podria reemplazar la i por el numero 1 y la a por el signo @,
+    // asi p1zz@,y aun seria perfectamente valido.
+
+// Funciones:
+    // Las funciones son prodecimientos que puedes guardar en la memoria
+    // para usarlos y repetirlos las veces que consideres necesario.
+    // Los mejor de las funciones es que con pocas palabras puedes
+    // hacer solicitudes que normalmente requeririan muchas mas palabras y tiempo.
+    // Por ejemplo: una vez que haz aprendido a hacer un sandwitch, no es necesario
+    // que cada vez que te pidan uno te expliquen como hacerlo,
+    // ya que solo con decirte si lo quieren con uno u otro ingrediente
+    // seras capaz de hacerlo por ti mismo.
+
+// Declaraciones if:
+    // Las declaraciones if son palabras con las que puedes establecer una condicion,
+    // que debe ser satisfecha para obtener el resultado que esperas.
+    // Por ejemplo: a veces tendras ganas de salir con tus amigos,
+    // pero tu mama solo lo permitira despues de que hagas tus deberes,
+    // por lo que se establece que; solo si haces tus deberes podras salir con tus amigos.
+
+// Valores booleanos:
+    // Los valores booleanos son sencillos.
+    // Solo tienen dos opciones, asi que es resultado sera siempre uno de los dos.
+    // Por ejemplo: un numero puede ser positivo o negativo,
+    // asi como una letra puede estar escrita en mayuscula o minuscula.
