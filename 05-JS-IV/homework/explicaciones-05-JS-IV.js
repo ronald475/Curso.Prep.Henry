9349efd383e4b473a242bd754dfb471aaae3e5b0
ronald475 <rdominguez2475@gmail.com>
2021-11-17 16:15:19 -0300

    Adding new file, 05-JS-IV/homework/explicaciones-05-JS-IV.js; updating homework 05-JS-IV/homework/homework.js

---
+++ 05-JS-IV/homework/explicaciones-05-JS-IV.js
@@ -0,0 +1,28 @@
+// Objetos
+    // Los objetos son cajas que sirven para guardar gran
+    // cantidad de informacion. Se parecen mucho a los arrays,
+    // pero la mayor diferencia entre ellos es que mientras
+    // los arrays tienen indices de orden numerico, los objetos
+    // se organizan de otra manera. Gracias a ello, la informacion
+    // que contienen no sigue un orden o estructura definida como
+    // en el caso de los arrays.
+
+// Propiedades
+    // Las propiedades son aquellas cosas que guardamos dentro de la
+    // caja que son los objetos. Pueden tener diferentes tipos, y se
+    // pueden utilizar para variedad de cosas. Cada objeto puede ademas
+    // contener propiedades  de distinto tipo, y estas propiedades a su
+    // vez pueden contener mas cosas en ellas.
+
+// Metodos
+    // Los metodos son funciones que permanecen guardadas dentro de los objetos.
+    // Se puede pensar en ellos como acciones que se van a tomar, teniendo
+    // como referencia aquello para lo que los diseñemos, y que una vez ejecutados
+    // mostraran resultados vinculados a la informacion contenida en la misma
+    // caja en la que fueron almacenados.
+    
+// Bucle for... in
+    // Parecido al bucle for ordinario, el for in es un bucle que puede
+    // ser utilizado como propiedad dentro de un objeto cuyo orden
+    // no incluye un indice numerico. Gracias a ello, nos ayuda a
+    // acceder a pares de clave-valor dentro de muchos objetos.
